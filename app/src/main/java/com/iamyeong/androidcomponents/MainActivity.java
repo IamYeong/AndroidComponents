bcc019129e0612fad0bb2aa6f9afaab1f0b1491a
IamYeong <wjdrhkddud2@gmail.com>
2021-02-27 21:15:17 +0900

    broadcastreceiver set

--- app/src/main/java/com/iamyeong/androidcomponents/MainActivity.java
+++ app/src/main/java/com/iamyeong/androidcomponents/MainActivity.java
@@ -2,13 +2,45 @@ package com.iamyeong.androidcomponents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyReceiver receiver;
+    private IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        receiver = new MyReceiver();
+        intentFilter = new IntentFilter();
+
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        registerReceiver(receiver, intentFilter);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(receiver);
+
     }
 }
--- app/src/main/java/com/iamyeong/androidcomponents/MyReceiver.java
+++ app/src/main/java/com/iamyeong/androidcomponents/MyReceiver.java
@@ -3,13 +3,15 @@ package com.iamyeong.androidcomponents;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        String action = intent.getAction();
+        Toast.makeText(context, "ACTION =>" + action, Toast.LENGTH_SHORT).show();
+
     }
 }
